9bd9dc842e663b13f948280c41e47b5a841690d3
Orthita <orthitabiswas@gmail.com>
2025-11-16 16:55:12 +0600

    after deleting and rearrage lab 1,for lab 2 5 task added

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const taskRouter = require('./routes/tasks');
 
 const app = express();
 
-// Middleware: JSON parser
+
 app.use(express.json());
 
-// Root route
+
 app.get('/', (req, res) => {
   res.send('Welcome to Task Manager API');
 });
 
-// Use the /tasks routes
+
 app.use('/tasks', taskRouter);
 
-// Error handler for invalid JSON
+
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     return res.status(400).json({ success: false, message: 'Invalid JSON format' });
@@ -22,6 +22,6 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// Start server
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
